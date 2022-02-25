9eba428c4218f4d337335506cc7f81881ebc2426
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2022-02-25 18:43:51 +0000

    Added CartIcon and CartDropdown components to the application, implemented cart reducer

--- src/index.js
+++ src/index.js
@@ -8,18 +8,20 @@ import store from './redux/store';
 import './index.css';
 import App from './App';
 
-// import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
