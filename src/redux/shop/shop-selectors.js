71c5c532038a8060c38fbef7ea039981d2057b1a
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2022-02-28 18:37:20 +0000

    Updated selectors and corresponding components with new redux flow for directory/shop

--- src/redux/shop/shop-selectors.js
+++ src/redux/shop/shop-selectors.js
@@ -1,14 +1,6 @@
 import { createSelector } from 'reselect';
 import memoize from 'lodash.memoize';
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-};
-
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -16,14 +8,17 @@ export const selectCollections = createSelector(
     shop => shop.collections
 );
 
+// get all the keys, map over that array of keys to get the value of collections object at that key, give an array of items 
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+);
+
 // find collection.id matching the url parameter of the collection id map
 // curried function - a function that returns a function
 export const selectCollection = memoize((collectionUrlParam) => 
     createSelector(
         [selectCollections],
-        collections => 
-            collections.find(
-                collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-            )
+        collections => collections[collectionUrlParam]
     )
 );
