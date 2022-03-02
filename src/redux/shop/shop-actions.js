3f72e9c44d0fd9f4cff27858fc51a1824d0211f9
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2022-03-02 18:31:58 +0000

    Updated redux structure with redux-thunk

--- src/redux/shop/shop-actions.js
+++ src/redux/shop/shop-actions.js
@@ -1,6 +1,41 @@
 import ShopActionTypes from './shop-types';
 
-export const updateCollections = (collectionsMap) => ({
-    type: ShopActionTypes.UPDATE_COLLECTIONS,
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+
+export const fetchCollectionsStart = () => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_START
+});
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
     payload: collectionsMap
 });
+
+export const fetchCollectionsFailure = errorMessage => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+    payload: errorMessage
+});
+
+// actual function that we pass into components to begin this process
+// if redux-thunk middleware is enabled, any time you attempt to dispatch
+// a function instead of an object, the middleware will call the function
+// with dispatch method itself as the first argument
+
+// return this function which can dispatch multiple actions
+// which in turn allows us to handle asynchronous activity inside of
+// a function instead of a component
+export const fetchCollectionsStartAsync = () => {
+    return dispatch => {
+        const collectionRef = firestore.collection('collections');
+        // the moment this function is called, dispatch fetchCollectionsStart()
+        dispatch(fetchCollectionsStart());
+        // then begin this asynchronous request
+        collectionRef
+            .get()
+            .then(snapshot => {
+                const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+                dispatch(fetchCollectionsSuccess(collectionsMap));
+        })
+        .catch(error => dispatch(fetchCollectionsFailure(error.message)));
+    }
+};
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist'; // allows the browser to cache the store depending on certain configurations
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import rootReducer from './root-reducer';
 
 // set up middlewares
-const middlewares = [];
+const middlewares = [thunk];
 
 if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
