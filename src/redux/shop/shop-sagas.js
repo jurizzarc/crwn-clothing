13a325f578cfb15650f3e32e6c831801490a5a7a
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2022-04-04 15:33:09 +0100

    Created a saga for cart

--- src/redux/shop/shop-sagas.js
+++ src/redux/shop/shop-sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
@@ -18,7 +18,7 @@ export function* fetchCollectionsAsync() {
     } catch (error) {
         yield put(fetchCollectionsFailure(error.message));
     }
-};
+}
 
 export function* fetchCollectionsStart() {
     // to issue this API call only once
@@ -27,4 +27,8 @@ export function* fetchCollectionsStart() {
         ShopActionTypes.FETCH_COLLECTIONS_START,
         fetchCollectionsAsync 
     );
-};
+}
+
+export function* shopSagas() {
+    yield all([call(fetchCollectionsStart)]);
+}
