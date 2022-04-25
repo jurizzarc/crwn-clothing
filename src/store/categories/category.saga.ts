486660e8f1560ba16747e7cc349a1aec39bfbff6
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2022-04-25 14:24:27 +0100

    Modify category, user, and root sagas over to typescript

--- src/store/categories/category.saga.ts
+++ src/store/categories/category.saga.ts
@@ -1,8 +1,8 @@
-import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'typed-redux-saga/macro';
 
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
 
-import { 
+import {
     fetchCategoriesSuccess,
     fetchCategoriesFailure
 } from './category.action';
@@ -11,20 +11,20 @@ import { CATEGORIES_ACTION_TYPES } from './category.types';
 
 export function* fetchCategoriesAsync() {
     try {
-        const categoriesArray = yield call(getCategoriesAndDocuments, 'collections');
-        yield put(fetchCategoriesSuccess(categoriesArray));
+        const categoriesArray = yield* call(getCategoriesAndDocuments);
+        yield* put(fetchCategoriesSuccess(categoriesArray));
     } catch (error) {
-        yield put(fetchCategoriesFailure(error));
+        yield* put(fetchCategoriesFailure(error as Error));
     }
 }
 
 export function* onFetchCategories() {
-    yield takeLatest(
+    yield* takeLatest(
         CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START,
-        fetchCategoriesAsync 
+        fetchCategoriesAsync
     );
 }
 
 export function* categoriesSaga() {
-    yield all([call(onFetchCategories)]);
+    yield* all([call(onFetchCategories)]);
 }
--- src/store/user/user.action.ts
+++ src/store/user/user.action.ts
@@ -1,3 +1,5 @@
+import { User } from 'firebase/auth';
+
 import { USER_ACTION_TYPES } from './user.types';
 import {
     createAction,
@@ -44,7 +46,7 @@ export type SignUpStart = ActionWithPayload<
 
 export type SignUpSuccess = ActionWithPayload<
     USER_ACTION_TYPES.SIGN_UP_SUCCESS, {
-        user: UserData,
+        user: User,
         additionalDetails: AdditionalInformation
     }
 >;
@@ -85,7 +87,7 @@ export const emailSignInStart = withMatcher(
 );
 
 export const signInSuccess = withMatcher(
-    (user: UserData): SignInSuccess =>
+    (user: UserData & { id: string }): SignInSuccess =>
         createAction(USER_ACTION_TYPES.SIGN_IN_SUCCESS, user)
 );
 
@@ -104,7 +106,7 @@ export const signUpStart = withMatcher(
 );
 
 export const signUpSuccess = withMatcher(
-    (user: UserData, additionalDetails: AdditionalInformation): SignUpSuccess =>
+    (user: User, additionalDetails: AdditionalInformation): SignUpSuccess =>
         createAction(USER_ACTION_TYPES.SIGN_UP_SUCCESS, { user, additionalDetails })
 );
 
