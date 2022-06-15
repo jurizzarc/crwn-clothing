b47c69c5759f8b8093af93c3eb1a234b7aab9c6e
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2022-06-15 20:10:17 +0100

    Type out button, form input, sign in and sign up components

--- src/components/form-input/form-input.styles.tsx
+++ src/components/form-input/form-input.styles.tsx
@@ -11,7 +11,11 @@ const shrinkLabelStyles = css`
     color: ${colours.main};
 `;
 
-export const FormInputLabel = styled.label`
+type FormInputLabelProps = {
+    shrink?: boolean;
+}
+
+export const FormInputLabel = styled.label<FormInputLabelProps>`
     color: $sub-color;
     font-size: 16px;
     font-weight: normal;
